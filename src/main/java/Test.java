6866d8d42f4f5513bf3dab5ed4a798e3f8615efc
linzy <linzy@shanhs.com>
2018-10-10 21:57:52 +0800

    add status

---
+++ src/main/java/Test.java
@@ -0,0 +1,11 @@
+import com.hkw.common.consts.status.DataStatus;
+
+/**
+ * @author linzy
+ * @verison 2018/8/22
+ */
+public class Test {
+    public static void main(String[] args) {
+        Integer date = DataStatus.TEST;
+    }
+}
